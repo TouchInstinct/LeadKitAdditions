8a54260c1ef84faa771d349fa179c8da927e33c8
nikAshanin <nv.ashanin@gmail.com>
2017-04-06 19:57:14 +0300

    update network service

---
+++ LeadKitAdditions/LeadKitAdditions/Classes/DefaultNetworkService.swift
@@ -0,0 +1,108 @@
+//
+//  Copyright (c) 2017 Touch Instinct
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the Software), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED AS IS, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//  THE SOFTWARE.
+//
+
+import Foundation
+import Alamofire
+import LeadKit
+import ObjectMapper
+import RxSwift
+import RxCocoa
+import RxAlamofire
+
+fileprivate let defaultTimeoutInterval = 20.0
+
+open class DefaultNetworkService: NetworkService {
+
+    static let sharedInstance = DefaultNetworkService()
+
+    static let retryLimit = 3
+
+    open class func baseUrl() -> String {
+        fatalError("base url should be overrided")
+    }
+
+    private override init(sessionManager: Alamofire.SessionManager) {
+        super.init(sessionManager: sessionManager)
+
+        bindActivityIndicator()
+    }
+
+    private convenience init() {
+        let configuration = URLSessionConfiguration.default
+        configuration.timeoutIntervalForRequest = defaultTimeoutInterval
+
+        let serverTrustPolicies: [String: ServerTrustPolicy] = [
+            DefaultNetworkService.baseUrl(): .disableEvaluation
+        ]
+
+        let sessionManager = SessionManager(configuration: configuration,
+                                            serverTrustPolicyManager: ServerTrustPolicyManager(policies: serverTrustPolicies))
+
+        self.init(sessionManager: sessionManager)
+    }
+
+    // MARK: - Internal methods
+
+    func request<T: ImmutableMappable>(with parameters: ApiRequestParameters) -> Observable<T> {
+        let apiResponseRequest = rxRequest(with: parameters) as Observable<(response: HTTPURLResponse, model: ApiResponse)>
+
+        return apiResponseRequest
+            .handleConnectionErrors()
+            .map {
+                if $0.model.errorCode == 0 {
+                    return try T(JSON: try cast($0.model.result) as [String: Any])
+                } else {
+                    throw ApiError(apiResponse: $0.model)
+                }
+            }
+            .handleGeneralApiErrors()
+    }
+
+    func requestForResult(with parameters: ApiRequestParameters) -> Observable<Bool> {
+        let apiResponseRequest = rxRequest(with: parameters) as Observable<(response: HTTPURLResponse, model: ApiResponse)>
+
+        return apiResponseRequest
+            .handleConnectionErrors()
+            .map {
+                if $0.model.errorCode == 0 {
+                    return true
+                } else {
+                    throw ApiError(apiResponse: $0.model)
+                }
+            }
+            .handleGeneralApiErrors()
+    }
+
+}
+
+extension ApiRequestParameters {
+
+    init(url: String, parameters: [String: Any] = [:]) {
+
+        self.init(url: URLSessionConfiguration.baseUrl() + url,
+                  method: .post,
+                  parameters: parameters,
+                  encoding: JSONEncoding.default,
+                  headers: nil)
+    }
+    
+}
---
+++ LeadKitAdditions/LeadKitAdditions/Extensions/Observable+Extensions.swift
@@ -0,0 +1,69 @@
+//
+//  Copyright (c) 2017 Touch Instinct
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the Software), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED AS IS, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//  THE SOFTWARE.
+//
+
+import RxSwift
+import Alamofire
+import CocoaLumberjack
+
+public typealias VoidBlock = () -> Void
+
+public extension Observable {
+
+    public func handleConnectionErrors() -> Observable<Observable.E> {
+        return observeOn(CurrentThreadScheduler.instance)
+
+            // handle no internet connection
+            .do(onError: { error in
+                if let urlError = error as? URLError,
+                    urlError.code == .notConnectedToInternet ||
+                        urlError.code == .timedOut {
+                    DDLogError("Error: No Connection")
+                    throw ConnectionError.noConnection
+                }
+            })
+
+            // handle unacceptable http status code like "500 Internal Server Error" and others
+            .do(onError: { error in
+                if let afError = error as? AFError,
+                    case let .responseValidationFailed(reason: reason) = afError,
+                    case let .unacceptableStatusCode(code: statusCode) = reason {
+                    DDLogError("Error: Unacceptable HTTP Status Code - \(statusCode)")
+                    throw ConnectionError.noConnection
+                }
+            })
+    }
+
+    public func retryWithinErrors(_ errorTypes: [Error.Type] = [ConnectionError.self],
+                           retryLimit: Int = SberCardsNetworkService.retryLimit)
+        -> Observable<Observable.E> {
+
+            return observeOn(CurrentThreadScheduler.instance)
+                .retryWhen { errors -> Observable<Observable.E> in
+                    return errors.flatMapWithIndex { e, a -> Observable<Observable.E> in
+                        let canRetry = errorTypes.contains { type(of: e) == $0 }
+
+                        return (canRetry && a < retryLimit - 1) ? self : .error(e)
+                    }
+            }
+    }
+
+}
