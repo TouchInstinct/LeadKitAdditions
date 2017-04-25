6efc671aea231b83b672e4d2fd72da7db0bca847
Alexey Gerasimov <gmunltd@gmail.com>
2017-04-25 17:59:41 +0300

    BasePassCodeService added

---
+++ LeadKitAdditions/LeadKitAdditions/Services/BasePassCodeService.swift
@@ -0,0 +1,99 @@
+//
+//  Copyright (c) 2017 Touch Instinct
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the Software), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED AS IS, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//  THE SOFTWARE.
+//
+
+import KeychainAccess
+import CocoaLumberjack
+
+open class BasePassCodeService {
+
+    open class var keychainService: String {
+        return Bundle.main.bundleIdentifier ?? ""
+    }
+
+    public init() {
+        let isInitialLoad = UserDefaults.standard.bool(forKey: Keys.isInitialLoad)
+        if isInitialLoad {
+            UserDefaults.standard.set(false, forKey: Keys.isInitialLoad)
+            reset()
+        }
+    }
+
+    // MARK: - Private stuff
+
+    fileprivate lazy var keychain: Keychain = {
+        return Keychain(service: keychainService)
+            .synchronizable(false)
+    }()
+
+    fileprivate var passCodeHash: String? {
+        return keychain[Keys.passCodeHash]
+    }
+
+    fileprivate enum Keys {
+        static let passCodeHash     = "passCodeHash"
+        static let isTouchIdEnabled = "isTouchIdEnabled"
+        static let isInitialLoad    = "isInitialLoad"
+    }
+
+    fileprivate enum Values {
+        static let touchIdEnabled = "touchIdEnabled"
+    }
+
+}
+
+extension BasePassCodeService {
+
+    public var isPassCodeSaved: Bool {
+        return keychain[Keys.passCodeHash] != nil
+    }
+
+    public var isTouchIdEnabled: Bool {
+        get {
+            return keychain[Keys.isTouchIdEnabled] == Values.touchIdEnabled
+        }
+        set {
+            keychain[Keys.isTouchIdEnabled] = newValue ? Values.touchIdEnabled : nil
+        }
+    }
+
+    public func save(passCode: String?) {
+        keychain[Keys.passCodeHash] = passCode?.hashMD5
+    }
+
+    public func check(passCode: String) -> Bool {
+        return passCode.hashMD5 == passCodeHash
+    }
+
+    public func reset() {
+        save(passCode: nil)
+        isTouchIdEnabled = false
+    }
+
+}
+
+private extension String {
+
+    var hashMD5: String? {
+        return self
+    }
+
+}
