17a5460009d28c50bad84049c2182a6d039de51f
Igor Kislyuk <igorkislyuk@icloud.com>
2017-06-06 14:26:33 +0300

    Fix. Remove synchronize

--- LeadKitAdditions/Sources/Services/BaseUserService.swift
+++ LeadKitAdditions/Sources/Services/BaseUserService.swift
@@ -58,7 +58,6 @@ open class BaseUserService {
     open class func clearData() {
         UserDefaults.standard.sessionId = nil
         UserDefaults.standard.userLogin = nil
-        UserDefaults.standard.synchronize()
     }
 
 }
