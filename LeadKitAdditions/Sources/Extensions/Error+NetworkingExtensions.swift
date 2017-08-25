6d51a8e0f5cda113c7e91f7c7a88561fb8085a26
Igor <Kislyuk>
2017-08-25 12:39:21 +0300

    Fix capitalization

--- LeadKitAdditions/Sources/Extensions/Error+NetworkingExtensions.swift
+++ LeadKitAdditions/Sources/Extensions/Error+NetworkingExtensions.swift
@@ -3,7 +3,7 @@ import Foundation
 /// Extension for network Error classification
 public extension Error {
 
-    /// Returns: TRUE if error is connection error
+    /// Returns: true if error is connection error
     var isConnectionError: Bool {
         guard let urlError = self as? URLError else {
             return false
@@ -12,7 +12,7 @@ public extension Error {
         return urlError.code == .notConnectedToInternet || urlError.code == .timedOut
     }
 
-    /// Returns: TRUE if server json response is not valid
+    /// Returns: true if server json response is not valid
     var isResponseSerializationError: Bool {
         return (self as? AFError)?.isResponseSerializationError ?? false
     }
